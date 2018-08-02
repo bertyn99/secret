042f4e60abc2d18335f8debe3b9d47cab4b651af
bertyn99 <bboulikou@gmail.com>
2018-08-02 18:26:06 +0200

    add modif gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 resources :user
 root to:'sessions#home', as:'home'
 get'/secret/:id',to:'sessions#show', as:'show'
-get'/login', to:'sessions#new', as:'signin'
+get'/login', to:'sessions#new', as:'login'
 post'/login', to:'sessions#create' 
-delete'/logout',to: 'session#destroy'
+delete'/logout',to: 'session#destroy', as:'logout'
 end
